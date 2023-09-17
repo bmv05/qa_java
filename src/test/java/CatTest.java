2da495bc96f1d475a5c8dc54050304438be59a9b
Maria Belova <bmvria357@gmail.com>
2023-09-17 21:18:54 +0300

    Добавила в cat тесты со стабом

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,27 +1,35 @@
 import com.example.Cat;
 import com.example.Feline;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    @Mock
+    Feline feline;
+
     @Test
-    public void getSoundTest(){
-        Cat cat = new Cat(new Feline());
+    public void getSoundReturnMeow() {
+        Cat cat = new Cat(feline);
         assertEquals("Мяу", cat.getSound());
     }
 
     @Test
-    public void getFoodTest(){
-        Cat cat = new Cat(new Feline());
+    public void getFoodReturnValueFromEatMeat() {
+        List<String> listOfFood = List.of("Грибы", "Огурцы", "Рыба");
         try {
-            assertEquals(List.of("Животные", "Птицы", "Рыба"),cat.getFood());
+            Mockito.when(this.feline.eatMeat()).thenReturn(listOfFood);
+            Cat cat = new Cat(this.feline);
+            assertEquals(listOfFood, cat.getFood());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
