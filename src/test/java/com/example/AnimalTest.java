b7e6091331d82bf097e28cbded57f3ca464ab440
Maria Belova <bmvria357@gmail.com>
2023-09-18 16:20:18 +0300

    Вынесла параметризованные тесты Animal в отдельный класс

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -1,41 +1,15 @@
 package com.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import java.util.List;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class AnimalTest {
-    List<String> food;
-    String animalKind;
-    Animal animal;
-
-    public AnimalTest(String animalKind, List<String> food) {
-        this.food = food;
-        this.animalKind = animalKind;
-        this.animal = new Animal();
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] listAnimal() {
-        return new Object[][]{
-                {"Травоядное", List.of("Трава", "Различные растения")},
-                {"Хищник", List.of("Животные", "Птицы", "Рыба")}
-        };
-    }
-
-    @Test
-    public void checkFood() throws Exception {
-        assertEquals(this.food, this.animal.getFood(this.animalKind));
-    }
-
 
     @Test
     public void checkExceptionGetFoof() {
+        Animal animal = new Animal();
         try {
-            this.animal.getFood("Error");
+            animal.getFood("Error");
         } catch (Exception e) {
             assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", e.getMessage());
         }
@@ -43,9 +17,7 @@ public class AnimalTest {
 
     @Test
     public void checkDefaultFamily() {
-        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи", this.animal.getFamily());
+        Animal animal = new Animal();
+        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи", animal.getFamily());
     }
-
 }
-
-
