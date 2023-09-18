bb977b79f3e8461d9a7f6b403908ef0c96138866
Maria Belova <bmvria357@gmail.com>
2023-09-18 16:13:16 +0300

    Вынесла параметризованные тесты Lion в отдельный класс

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,45 +1,25 @@
 package com.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class LionTest {
     Lion lion;
-    boolean hasMane;
-    List<String> food;
 
-    public LionTest(String sex, boolean hasMane, List<String> food) throws Exception {
-        this.hasMane = hasMane;
-        this.food = food;
-        this.lion = new Lion(sex, new Feline());
+    public LionTest() throws Exception {
+        this.lion = new Lion("Самец", new Feline());
     }
 
-    @Parameterized.Parameters
-    public static Object[][] listMale() {
-        List<String> food = List.of("Животные", "Птицы", "Рыба");
-        return new Object[][]{
-                {"Самец", true, food},
-                {"Самка", false, food}
-        };
-    }
     @Test
     public void getKittensReturn1() {
         assertEquals(1, this.lion.getKittens());
     }
 
     @Test
-    public void doesLionHaveMane() {
-        assertEquals(this.hasMane, this.lion.doesHaveMane());
-    }
-
-    @Test
     public void getFood() {
         try {
-            assertEquals(this.food, this.lion.getFood());
+            assertEquals(List.of("Животные", "Птицы", "Рыба"), this.lion.getFood());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
